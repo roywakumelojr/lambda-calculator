9cf61a98670dff34b5e87134d908080bade3e5f3
Roy Wakumelo Jr <roywakumelo@roys-mbp.attlocal.net>
2019-09-11 11:21:26 -0400

    MVP Has Been Reached

--- src/components/ButtonComponents/NumberButtons/NumberButton.js
+++ src/components/ButtonComponents/NumberButtons/NumberButton.js
@@ -1,9 +1,13 @@
 import React from "react";
 
-const NumberButton = () => {
+const NumberButton = (props) => {
   return (
-    <>
+
+    <button className="button-number">
       {/* Display a button element rendering the data being passed down from the parent container on props */}
-    </>
+      {props.numbers}
+    </button>
+    
   );
 };
+export default NumberButton;
--- src/components/ButtonComponents/OperatorButtons/OperatorButton.js
+++ src/components/ButtonComponents/OperatorButtons/OperatorButton.js
@@ -1,9 +1,11 @@
 import React from "react";
 
-const OperatorButton = () => {
+const OperatorButton = (props) => {
   return (
-    <>
+    <button className="button-operator">
       {/* Display a button element rendering the data being passed down from the parent container on props */}
-    </>
+      {props.operators.char}
+    </button>
   );
 };
+export default OperatorButton;
